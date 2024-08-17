cdf486107d99f17b6a5774bda0931f8b3319903e
Stefan Maier <stefan.maier.98@t-online.de>
2024-08-17 21:51:05 +0200

    feat(Encode): Programm logic for encoding the string

--- src/RailFenceCipher/Program.cs
+++ src/RailFenceCipher/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Cipher.RailFence;
+
+static public class RailFenceCipher
+{
+    static void Main() { }
+    public static string Encode(string s, int n)
+    {
+        if (String.IsNullOrEmpty(s)) { return String.Empty; }
+        var output = String.Empty;
+        for (int i = 0; i < n; i++)
+        {
+            int cycle = 0;
+            var indexes = CalculateIndexes(cycle, n, i);
+
+            while (indexes.First < s.Length)
+            {
+                cycle++;
+                if (indexes.First == indexes.Second)
+                {
+                    output += s[indexes.First].ToString();
+                }
+                else
+                {
+                    if (IsValidIndex(indexes.First, s) && IsNotMaxDepth(i, n)) { output += s[indexes.First].ToString(); }
+                    if (IsValidIndex(indexes.Second, s)) { output += s[indexes.Second].ToString(); }
+                }
+
+                indexes = CalculateIndexes(cycle, n, i);
+            }
+        }
+        return output;
+    }
+    private static (int First, int Second) CalculateIndexes(int cycle, int depth, int currentline)
+    {
+        return (First: CalculateFirstIndex(cycle, depth, currentline), Second: CalculateSecondIndex(cycle, depth, currentline));
+    }
+    //currentDepth starts at 0
+    private static bool IsNotMaxDepth(int currentDepth, int maxDepth) => maxDepth != currentDepth + 1;
+
+    private static int CalculateSecondIndex(int cycle, int depth, int currentline)
+    {
+        return cycle * (2 * depth - 2) + currentline;
+    }
+    private static int CalculateFirstIndex(int cycle, int depth, int currentline)
+    {
+        return cycle * (2 * depth - 2) - currentline;
+    }
+    private static bool IsValidIndex(int index, string text)
+    {
+        if (index < 0) { return false; }
+        if (index > text.Length - 1) { return false; }
+        return true;
+    }
+
+    public static string Decode(string s, int n)
+    {
+        if (String.IsNullOrEmpty(s)) { return String.Empty; }
+        return String.Empty;
+    }
+}
--- test/RailFenceCipherTests/UnitTest1.cs
+++ test/RailFenceCipherTests/UnitTest1.cs
@@ -1,15 +1,37 @@
-namespace RailFenceCipherTests;
+using Cipher.RailFence;
 
-public class Tests
+public class SolutionTest
 {
-    [SetUp]
-    public void Setup()
+    [Test]
+    public void EncodeSampleTests()
     {
+        string[][] encodes =
+        {
+            new[] { "WEAREDISCOVEREDFLEEATONCE", "WECRLTEERDSOEEFEAOCAIVDEN" },  // 3 rails
+            new[] { "Hello, World!", "Hoo!el,Wrdl l" },    // 3 rails
+            new[] { "Hello, World!", "H !e,Wdloollr" },    // 4 rails
+            new[] { "", "" }                               // 3 rails (even if...)
+        };
+        int[] rails = { 3, 3, 4, 3 };
+        for (int i = 0; i < encodes.Length; i++)
+        {
+            Assert.That(RailFenceCipher.Encode(encodes[i][0], rails[i]), Is.EqualTo(encodes[i][1]));
+        }
     }
 
     [Test]
-    public void Test1()
+    public void DecodeSampleTests()
     {
-        Assert.Pass();
+        string[][] decodes =
+        {
+            new[] { "WECRLTEERDSOEEFEAOCAIVDEN", "WEAREDISCOVEREDFLEEATONCE" },    // 3 rails
+            new[] { "H !e,Wdloollr", "Hello, World!" },    // 4 rails
+            new[] { "", "" }                               // 3 rails (even if...)
+        };
+        int[] rails = { 3, 4, 3 };
+        for (int i = 0; i < decodes.Length; i++)
+        {
+            Assert.That(RailFenceCipher.Decode(decodes[i][0], rails[i]), Is.EqualTo(decodes[i][1]));
+        }
     }
 }
